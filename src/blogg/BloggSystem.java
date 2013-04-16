b6f47686e5634dabc7d58201fd257f1175a29cce
Cecilia Saming Nilsson <Cissi@Cecilias-MacBook-Pro.local>
2013-04-16 13:12:35 +0200

    jade jdae

--- src/blogg/BloggSystem.java
+++ src/blogg/BloggSystem.java
@@ -9,6 +9,8 @@ import java.util.*;
 
 public class BloggSystem {
 	public List<Post> posts = new ArrayList<Post>();
+	public List<Writer> Writers = new ArrayList<Writer>();
+	public List<Category> Categories = new ArrayList<Category>(); 
 
 	Connection con = null;
 	Statement st = null;
@@ -16,18 +18,40 @@ public class BloggSystem {
 
 	String url = "jdbc:mysql://localhost:3306/Bloggen";
 	String user = "root";
-	String password = "";
+	String password = "root";
 
 	public void fill() {
 		try {
 			con = DriverManager.getConnection(url, user, password);
+			
+			st = con.createStatement();
+			rs = st.executeQuery("select * from categories");
+
+			while (rs.next()) {
+				Category category = new Category();
+				category.setName(rs.getString("name"));
+				Categories.add(category);
+
+			}
+			
+			st = con.createStatement();
+			rs = st.executeQuery("select * from writers");
+
+			while (rs.next()) {
+				Writer writer = new Writer();
+				writer.setName(rs.getString("name"));
+				Writers.add(writer);
+
+			}
+			
+			
 			st = con.createStatement();
 			rs = st.executeQuery("select * from posts");
 
 			while (rs.next()) {
 				Post blogItem = new Post();
 				blogItem.setTitle(rs.getString("title"));
-				
+				blogItem.setContent(rs.getString("post"));
 				posts.add(blogItem);
 
 			}
